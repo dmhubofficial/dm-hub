3e2756b4324e8b2bd827d53fe46f4533fc5bce12
dmhubofficial <aakibmd1122@gmail.com>
2026-09-24 15:33:10 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
-const express=require("express");const path=require("path");const app=express();const PORT=process.env.PORT||3000;
-app.use(express.json());app.use(express.static(path.join(__dirname,"public")));
-app.get("/{*splat}",(req,res)=>res.sendFile(path.join(__dirname,"public","index.html")));
-app.listen(PORT,()=>console.log(`DM Hub running on http://localhost:${PORT}`));
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`DM Hub running on http://localhost:${PORT}`);
+});
